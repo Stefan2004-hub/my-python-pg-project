57f133e963daca93ec384ee2257c5bceeb5931c5
Dumitriu Stefan <d.stef@yahoo.com>
2026-04-05 03:49:51 +0300

    refactor the __init__.py file Base class

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,20 +4,19 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
+    from app.models.category import Category
+    from app.models.customer import Customer
+    from app.models.order import Order
+    from app.models.order_item import OrderItem
+    from app.models.product import Product
+
     """Base class for all ORM models."""
 
-
-from app.models.category import Category
-from app.models.customer import Customer
-from app.models.order import Order
-from app.models.order_item import OrderItem
-from app.models.product import Product
-
-__all__ = [
-    "Base",
-    "Category",
-    "Customer",
-    "Order",
-    "OrderItem",
-    "Product",
-]
+    __all__ = [
+        "Base",
+        "Category",
+        "Customer",
+        "Order",
+        "OrderItem",
+        "Product",
+    ]
